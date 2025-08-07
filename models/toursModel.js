f1a6ea4cc99bd9496f4626ba573022276851a1b5
TabishQamar045 <tabishqamar5961@gmail.com>
2025-08-08 03:38:46 +0500

    Push:New aggreagte middleware is added

--- models/toursModel.js
+++ models/toursModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'A tour must have a duration']
@@ -51,6 +52,10 @@ const tourSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
     select: false
+  },
+  secretTour:{
+    type:Boolean,
+    default:false
   }
 }, {
   toJSON: { virtuals: true },
@@ -60,6 +65,38 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function(){
   return this.duration/7;
 })
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save',function(next){
+  this.slug=slugify(this.name, {lower:true});
+  next();
+})
+
+tourSchema.post('save',function(doc,next){
+  console.log(doc);
+  next();
+})
+
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/,function(next){
+  this.find({secretTour:{$ne:true}});
+  next();
+})
+
+tourSchema.post(/^find/,function(docs,next){
+  console.log(docs);
+  next();
+})
+
+// AGGREGATION MIDDLEWARE
+
+tourSchema.pre('aggregate',function(next){
+  console.log(this.pipeline());
+  this.pipeline().unshift({$match:{secretTour:{$ne:true}}});
+  next();
+})
+
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -14,10 +14,15 @@ const router = express.Router();
 // router.param('id', checkId);
 
 
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+// Aliased routes
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+// Stats routes
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
+
+// Standard routes
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
